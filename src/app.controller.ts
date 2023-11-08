41413dc7baa65d910a0ad61c65a17e86ccdce722
Valerian Liparteliani <v.liparteliani@adjarabet.com>
2023-11-08 14:51:19 +0400

    init commit

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+
+@ApiTags()
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @ApiResponse({ status: 200, description: 'Returns hello world' })
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { config } from 'dotenv';
+config();
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const port = +process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Rest Api')
+    .setVersion('1.0')
+    .setExternalDoc('Postman Collection', '/api-json')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
+  console.log(`Application is running on port ${port}`);
+}
+bootstrap();
