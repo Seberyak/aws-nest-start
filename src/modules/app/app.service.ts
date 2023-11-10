33fde4e8602d4751b6fa33adfaee5bc923de3390
Valerian Liparteliani <v.liparteliani@adjarabet.com>
2023-11-10 14:01:43 +0400

    test ci/cd

--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -6,7 +6,6 @@ export class AppService {
   constructor(private readonly db: DocumentClient) {}
   async getHello(): Promise<any> {
     const res = await this.db.scan({ TableName: 'TestTable' }).promise();
-
     return { data: res.Items, status: 200, success: true };
   }
 }
