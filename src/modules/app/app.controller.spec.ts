db6cf66a7ed527e358e8704c26e5493a5e1b3ef5
Valerian Liparteliani <v.liparteliani@adjarabet.com>
2023-11-10 14:13:19 +0400

    fixed unit tests

--- src/modules/app/app.controller.spec.ts
+++ src/modules/app/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +9,12 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, DocumentClient],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appController = app.get(AppController);
   });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+  it('should be defined', () => {
+    expect(appController).toBeDefined();
   });
 });
