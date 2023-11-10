13ba3570dd96828b4b4016e2a1752b62edfe857b
Valerian Liparteliani <v.liparteliani@adjarabet.com>
2023-11-10 14:16:21 +0400

    fixed unit tests

--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -6,6 +6,7 @@ export class AppService {
   constructor(private readonly db: DocumentClient) {}
   async getHello(): Promise<{ data: any; status: number; success: boolean }> {
     const res = await this.db.scan({ TableName: 'TestTable' }).promise();
+
     return { data: res.Items, status: 200, success: true };
   }
 }
