0df78ca8968fef30087b55ede3d1aac0db9e3265
Valerian Liparteliani <v.liparteliani@adjarabet.com>
2023-11-10 14:03:38 +0400

    test ci/cd

--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -4,7 +4,7 @@ import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 @Injectable()
 export class AppService {
   constructor(private readonly db: DocumentClient) {}
-  async getHello(): Promise<any> {
+  async getHello(): Promise<{ data: any; status: number; success: boolean }> {
     const res = await this.db.scan({ TableName: 'TestTable' }).promise();
     return { data: res.Items, status: 200, success: true };
   }
